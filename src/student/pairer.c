531362e4d34e08fec2689da3ccebe1575a23d045
Enzo Eiki Yasuda <enzo.ey2003@gmail.com>
2026-06-14 18:32:28 -0300

    Update pairer.c

--- src/student/pairer.c
+++ src/student/pairer.c
@@ -4,32 +4,18 @@ int student_pair_syscall(struct syscall_pairer *pairer,
                          const struct syscall_event *ev,
                          struct syscall_event *out)
 {
-    static struct syscall_event entry_saved;
-    static int has_entry = 0;
-    
-    if(ev->entering) 
+    if (ev->entering)
     {
-        entry_saved = *ev;
-        has_entry = 1;
-        
+        pairer->entry = *ev;
+        pairer->has_entry = 1;
         return 0;
     }
     
-    if(!ev->entering)
-    {
-        if(!has_entry || entry_saved.syscall_no != ev->syscall_no)
-        {
-            return -1;
-        }
-        
-        *out = entry_saved;
-        out->ret = ev->ret;
-        out->entering = 0;
-        
-        has_entry = 0;
-        
-        return 1;
-    }
-    
-    return -1;
+    if (!pairer->has_entry || pairer->entry.syscall_no != ev->syscall_no)
+        return -1;
+    *out = pairer->entry;
+    out->ret = ev->ret;
+    out->entering = 0;
+    pairer->has_entry = 0;
+    return 1;
 }
